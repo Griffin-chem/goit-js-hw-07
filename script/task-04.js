928972e9de27107584d1eda49cadac0312e2fba2
Griffin <chemist_khnu@rambler.ru>
2020-05-19 23:49:46 +0300

    JS HW07 updated (task-04)

--- script/task-04.js
+++ script/task-04.js
@@ -1,15 +1,29 @@
 "use strict"
-const increaseBtn = document.querySelector('#counter button[data-action="increment"]');
-const decreaseBtn = document.querySelector('#counter button[data-action="decrement"]');
+const counterBtn = document.querySelector('#counter');
+// const decreaseBtn = document.querySelector('#counter button[data-action="decrement"]');
 const resultField = document.querySelector('#value');
-let value = Number(resultField.textContent);
-const increaseValue = function () {
-  value++;
-  resultField.textContent = value;
+const counter = {
+  value: 0,
+  increment() {
+    this.value += 1;
+  },
+  decrement() {
+    this.value -= 1;
+  },
 };
-const decreaseValue = function () {
-  value--;
-  resultField.textContent = value;
-};
-increaseBtn.addEventListener('click', increaseValue);
-decreaseBtn.addEventListener('click', decreaseValue);
+const changeValue = ({
+  target
+}) => {
+  counter[target.dataset.action]();
+  resultField.textContent = counter.value;
+}
+// const increaseValue = function () {
+//   value++;
+//   resultField.textContent = value;
+// };
+// const decreaseValue = function () {
+//   value--;
+//   resultField.textContent = value;
+// };
+counterBtn.addEventListener('click', changeValue);
+// decreaseBtn.addEventListener('click', changeValue);
